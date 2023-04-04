5e60b65c0a10cfe37ffb44a8237efd6b8c0ee2ff
Daniel <69496130+ZESI158@users.noreply.github.com>
2023-04-04 17:10:50 -0500

    Third class -home work
    EL trabajo no esta del todo bien ya que presenta muchas fallas debido a que no tenia  internet para investigar sobre un poco de lo que estaba haciendo

--- dino_runner/components/obstacles/obstacle_manage.py
+++ dino_runner/components/obstacles/obstacle_manage.py
@@ -1,28 +1,42 @@
 import pygame
-from dino_runner.components.obstacles.captus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.captus import Cactus,  Bird
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
+        
     def generate_obstacle(self):
         obstacle = Cactus(SMALL_CACTUS)
         return obstacle
+    def generate_obstacle1(self):
+        obstacle1 = Cactus(LARGE_CACTUS)
+        return obstacle1
+    def generate_obstacle2(self):
+        obstacle1 = Bird(BIRD)
+        return obstacle1
+
     
     def update(self, game):
-        if len(self.obstacles) ==0:
+        if len(self.obstacles) == 0:
             obstacle = self.generate_obstacle()
+            obstacle1 = self.generate_obstacle1()
+            obstacle2 = self.generate_obstacle2()
             self.obstacles.append(obstacle)
-
+            self.obstacles.append(obstacle1)
+            self.obstacles.append(obstacle2)
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 print("collision")
                 pygame.time.delay(1000)
                 game.playing = False
-                break
+        
 #implementar catus  grandes y aves altura 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+
+
